e2f0217e868e59d08753f9dbff72e71cd9987466
Murtala-Dahiru <murtaladahirukawu@gmail.com>
2025-06-16 02:33:05 +0100

    🔧 Fix Vite env variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'; // ✅ THIS is the missing line
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fnsrdflqlkmjznvrulcu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuc3JkZmxxbGttanpudnJ1bGN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMTk2NTQsImV4cCI6MjA2NTU5NTY1NH0.AQ_ayPAO_Qt9aK-52QuX_eYgLFN1KSlwV2vvzKXeZbk';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
